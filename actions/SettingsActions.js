0d4553224206ce1f1bc2772b5a8e4b3dfa755597
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-05-28 20:26:45 -0400

    added more actions

--- actions/SettingsActions.js
+++ actions/SettingsActions.js
@@ -1,28 +1,83 @@
-export { Actions } from "react-native-router-flux";
-import { SETTINGS_UPDATE, REMOVE_WORD, RESET_WORDS, NEW_GAME, GENERATE_WORD } from "./types";
+export { Actions } from 'react-native-router-flux';
+import {
+    SETTINGS_UPDATE,
+    REMOVE_WORD,
+    RESET_WORDS,
+    NEW_GAME,
+    SELECT_WORD,
+    UNSELECT_WORD,
+    APP_CLOSED,
+    CREATE_WORD_LIST,
+    DELETE_WORD_LIST,
+    CLEAR_WORDS,
+    OPEN_FIRST_TIME
+} from './types';
 
 export const settingUpdate = ({ prop, value }) => {
-  return {
-    type: SETTINGS_UPDATE,
-    payload: { prop, value }
-  };
+    return {
+        type: SETTINGS_UPDATE,
+        payload: { prop, value }
+    };
 };
 
 export const removeWord = playWord => {
-  return {
-    type: REMOVE_WORD,
-    payload: playWord
-  };
+    return {
+        type: REMOVE_WORD,
+        payload: playWord
+    };
 };
 
 export const resetWords = () => {
-  return {
-    type: RESET_WORDS
-  };
+    return {
+        type: RESET_WORDS
+    };
+};
+
+export const selectWordList = payload => {
+    return {
+        type: SELECT_WORD,
+        payload
+    };
+};
+export const unselectWordList = payload => {
+    return {
+        type: UNSELECT_WORD,
+        payload
+    };
 };
 
 export const newGame = () => {
-  return {
-    type: NEW_GAME
-  };
+    return {
+        type: NEW_GAME
+    };
+};
+export const clearWords = () => {
+    return {
+        type: CLEAR_WORDS
+    };
+};
+
+export const appClosed = () => {
+    return {
+        type: APP_CLOSED
+    };
+};
+
+export const openFirstTime = () => {
+    return {
+        type: OPEN_FIRST_TIME
+    };
+};
+
+export const createWordList = payload => {
+    return {
+        type: CREATE_WORD_LIST,
+        payload
+    };
+};
+export const deleteWordList = payload => {
+    return {
+        type: DELETE_WORD_LIST,
+        payload
+    };
 };
--- actions/types.js
+++ actions/types.js
@@ -2,10 +2,19 @@ export const SETTINGS_UPDATE = 'settings_update';
 
 export const GAIN_POINT = 'gain_point';
 export const LOSE_POINT = 'lose_point';
-export const ROUND_UPDATE = 'round_update'; 
+export const ROUND_UPDATE = 'round_update';
 
-export const GAME_UPDATE = 'game_update'
-export const NEW_GAME = 'new_game'
+export const GAME_UPDATE = 'game_update';
+export const NEW_GAME = 'new_game';
 
-export const REMOVE_WORD = 'remove_word'
-export const RESET_WORDS = 'reset_words'
+export const REMOVE_WORD = 'remove_word';
+export const RESET_WORDS = 'reset_words';
+export const CLEAR_WORDS = 'clear_words';
+
+export const SELECT_WORD = 'select_word';
+export const UNSELECT_WORD = 'unselect_word';
+export const CREATE_WORD_LIST = 'create_word_list';
+export const DELETE_WORD_LIST = 'delete_word_list';
+
+export const APP_CLOSED = 'app_closed';
+export const OPEN_FIRST_TIME = 'open_first_time';
