c0e1353399913c507b819e9941528b7748bbb093
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-07-02 19:53:13 -0400

    remove reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import wordReducer from './wordReducer.js'
 import settingReducer from './settingReducer.js'
 
 
 export default combineReducers({
-    play:wordReducer,
     setting:settingReducer
 })
